18b1a9c56d09d131bd22c77a8e2653b98e57ee6d
Destinee A. Adams <adestinee37@gmail.com>
2023-02-24 12:57:51 -0500

    login

---
+++ main.py
@@ -0,0 +1,24 @@
+import os, time
+from playwright.sync_api import sync_playwright 
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False, slow_mo=50)
+    page = browser.new_page()
+
+    # login page nav
+    page.goto("https://cisleads.com/?login=1")
+
+    # login
+    page.fill('input#UserName',  os.getenv("UN"))
+    page.fill('input#Password',  os.getenv("PW"))
+    page.click('#buttonLogin')
+    time.sleep(5)
+
+    # start serach 
+    page.goto("https://cisleads.com/desktop")
+    page.get_by_title('GCs & Subs').click()
+
+    time.sleep(5)
